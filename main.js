91c8caf75597ad915669ec7f7a87e1b6d033e780
sandinius <santiagoandin@gmail.com>
2023-11-28 11:35:02 -0300

    Preentrega2SantiagoAndini

--- main.js
+++ main.js
@@ -46,6 +46,37 @@ class ProductManager {
     
         return product;
       }
+      updateProduct({ id, title, description, price, thumbnail, code, stock }) {
+        const productIndex = this.products.findIndex(product => product.id === id);
+      
+        if (productIndex !== -1) {
+          this.products[productIndex] = {
+            id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+          };
+      
+          return this.products[productIndex];
+        } else {
+          throw new Error("Error: Product not found");
+        }
+      }
+      
+      deleteProduct(id) {
+        const productIndex = this.products.findIndex(product => product.id === id);
+      
+        if (productIndex !== -1) {
+          const deletedProduct = this.products.splice(productIndex, 1)[0];
+          return deletedProduct;
+        } else {
+          throw new Error("Error: Product not found");
+        }
+      }
+      
     }
 
 const productManager = new ProductManager();
@@ -85,6 +116,23 @@ console.log(productManager.getProductById(addedProduct.id));
 try {
   productManager.getProductById("invalidId");
 } catch (error) {
-    console.log('Get produt by an invalid ID')
+    console.log('Get product by an invalid ID')
   console.error(error.message);
 }
+const updatedProduct = productManager.updateProduct({
+  id: addedProduct.id,
+  title: "producto prueba actualizado",
+  description: "Este es un producto prueba actualizado",
+  price: 600,
+  thumbnail: "Sin imagen",
+  code: "def456",
+  stock: 50
+});
+
+console.log('Product Updated:')
+console.log(productManager.getProducts());
+
+console.log("Product deleted");
+productManager.deleteProduct(updatedProduct.id);
+console.log(productManager.getProducts());
+
